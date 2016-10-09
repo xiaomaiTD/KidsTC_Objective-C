5fd8a9524942d6346948cee00d8abca992782655
zhanping <zhpiwy@163.com>
2016-10-09 09:17:31 +0800

    添加埋点管理者BuryPointManager

---
+++ KidsTC/KidsTC/Business/Global/BuryPoint/BuryPointManager.h
@@ -0,0 +1,13 @@
+//
+//  BuryPointManager.h
+//  KidsTC
+//
+//  Created by 詹平 on 2016/10/8.
+//  Copyright © 2016年 zhanping. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BuryPointManager : NSObject
+
+@end
---
+++ KidsTC/KidsTC/Business/Global/BuryPoint/BuryPointManager.m
@@ -0,0 +1,13 @@
+//
+//  BuryPointManager.m
+//  KidsTC
+//
+//  Created by 詹平 on 2016/10/8.
+//  Copyright © 2016年 zhanping. All rights reserved.
+//
+
+#import "BuryPointManager.h"
+
+@implementation BuryPointManager
+
+@end
